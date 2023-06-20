3cbd3b7dc0dad767e7d1cd8cbe43f9641ce242a1
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-20 18:26:10 -0500

    linters fix

--- person.rb
+++ person.rb
@@ -1,7 +1,8 @@
-require "./nameable.rb"
+require './nameable'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', _parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,11 +1,9 @@
-require "./decorator.rb"
+require './decorator'
 
 class TrimmerDecorator < Decorator
-    def correct_name
-        name = @nameable.correct_name
-        if name.length > 10
-            name = name[0..9]
-        end
-        name
-    end
+  def correct_name
+    name = @nameable.correct_name
+    name = name[0..9] if name.length > 10
+    name
+  end
 end
