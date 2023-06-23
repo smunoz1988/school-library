b6d39e52b36cbc816a762f0045afa1249910ea8c
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-23 18:33:31 -0500

    modify getter and setters

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :classroom #  considering attr?reader
 
   def initialize(name, age, classroom: nil, _parent_permission: true)
     super(age, name, _parent_permission: true)
@@ -12,7 +12,7 @@ class Student < Person
     '¯(ツ)/¯'
   end
 
-  def classroom_student(classroom)
+  def classroom_student=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
