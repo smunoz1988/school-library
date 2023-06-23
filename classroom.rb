b4200bec5404834f0322b5a3082a9503c739ebe3
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-22 21:15:59 -0500

    add classroom into student

--- classroom.rb
+++ classroom.rb
@@ -1,8 +1,14 @@
 class ClassRoom
-    attr_accessor :label
-    
-    def initialize(label)
-        @label = label
-    end
-    
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require './person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', _parent_permission: true)
+  attr_accessor :classroom
+
+  def initialize(age, classroom: nil, name = 'Unknown', _parent_permission: true)
     super(age, name, _parent_permission: true)
     @classroom = classroom
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
