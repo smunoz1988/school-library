a6ae2b406f10d354a19408e391a4579e4a464ef1
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-24 10:11:23 -0500

    create main.rb and add Interface class

--- main.rb
+++ main.rb
@@ -1,2 +1,12 @@
-require_relative 'app'
+require_relative './app'
 
+class Interface
+    def initialize
+        @app = App.new(self)
+
+        puts
+        puts 'Welcome to School Library System!'
+        # here should deply the main menu
+    end
+
+    
