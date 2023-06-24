30bdd407906ae1e3215e1d13edf94a0ec5e30cd6
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-24 10:16:48 -0500

    add option_selected method to send request to app

--- main.rb
+++ main.rb
@@ -9,4 +9,42 @@ class Interface
         # here should deply the main menu
     end
 
-    
+    def display_menu
+        puts
+        puts 'Please select an option by entering a number:'
+        puts '1. List all books'
+        puts '2. List all people'
+        puts '3. Create a person'
+        puts '4. Create a book'
+        puts '5. Create a rental'
+        puts '6. List all rentals for a given person id'
+        puts '7. Exit'
+        puts
+        user_choice = gets.chomp
+        # call the method that runs the user choice
+    end
+
+    def option_selected(user_choice)
+        options = {
+            '1' => :list_all_books,
+            '2' => :list_all_people,
+            '3' => :create_person,
+            '4' => :create_book,
+            '5' => :create_rental,
+            '6' => :list_all_rentals,
+            '7' => :exit
+        }
+
+        method = options[user_choice]
+        if method.nil?
+            puts 'Invalid option'
+            puts
+            display_menu
+        else
+            @app.send(method)
+        end
+    end
+end
+
+
+
