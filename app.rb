4e3529286f5a6e234eb5b845fd3d694a9dc93e21
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-23 19:20:36 -0500

    add list_books method

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
+require './person'
+require './book'
+require './rental'
+require './student'
+require './teacher'
+
 class App
-    def initialize
+    def initialize(mainCall)
+        @mainCall = mainCall
         @people = []
         @books = []
         @rentals = []
     end
+
+    def list_books
+        if @books.empty?
+            puts 'No books available'
+        else
+            puts 'List of books:'
+            @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+        end
+        # here will call the main menu
 end
