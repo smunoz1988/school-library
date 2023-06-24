d2556d895263497ee953bccfbd40c02059aee225
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-23 20:25:16 -0500

    add id as a parameter for rentals

--- app.rb
+++ app.rb
@@ -85,10 +85,12 @@ class App
         puts 'Select a person from the following list by number (not ID)'
         @people.each_with_index { |person, index| puts "#{index} [#{person.class} Name: #{person.name}, ID: #{person.id}", "Age: #{person.age}]" }
         person_index = gets.chomp.to_i
+        person_index = @people[person_index].id
         puts 'Date:'
         date = gets.chomp
         @rentals.push(Rental.new(date, @books[book_index], @people[person_index]))
         puts 'Rental created successfully'
         # here will call the main menu
     end
+    
 end
