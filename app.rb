4558df28f9b704542873e2df521d049d49b18e57
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-23 19:31:42 -0500

    create list_people method

--- app.rb
+++ app.rb
@@ -20,4 +20,15 @@ class App
             @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
         end
         # here will call the main menu
+    end
+
+    def list_people
+        if @people.empty?
+            puts 'No people available'
+        else
+            puts 'List of people:'
+            @people.each { |person| puts "[#{person.class} Name: #{person.name}, ID: #{person.id}", "Age: #{person.age}]" }
+        end
+        # here will call the main menu
+    end
 end
