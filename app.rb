2434d26f3cc597d5753a5f589661768d486f07fa
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-24 12:40:16 -0500

    change output messages to match required app

--- app.rb
+++ app.rb
@@ -88,7 +88,7 @@ class App
     puts
     puts 'Select a person from the following list by number (not ID)'
     @people.each_with_index do |person, index|
-      puts "#{index} [#{person.class} Name: #{person.name}, ID: #{person.id}", "Age: #{person.age}]"
+      puts "#{index} [#{person.class}] Name: #{person.name}, ID: #{person.id}", "Age: #{person.age}"
     end
     person_index = gets.chomp.to_i
     puts 'Date:'
@@ -115,6 +115,6 @@ class App
   end
 
   def quit
-    puts 'Good bye!'
+    puts 'Thank you for using this app!'
   end
 end
--- main.rb
+++ main.rb
@@ -5,13 +5,13 @@ class Interface
     @app = App.new(self)
 
     puts
-    puts 'Welcome to School Library System!'
+    puts 'Welcome to School Library App!'
     display_menu
   end
 
   def display_menu
     puts
-    puts 'Please select an option by entering a number:'
+    puts 'Please choose an option by entering a number:'
     puts '1. List all books'
     puts '2. List all people'
     puts '3. Create a person'
