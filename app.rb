21c49210100575b1214af40d5ea4d8a160b3374d
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-23 20:19:27 -0500

    add create_rental method

--- app.rb
+++ app.rb
@@ -78,4 +78,17 @@ class App
         # here will call the main menu
     end
 
+    def create_rental
+        puts 'Select a book from the following list by number'
+        @books.each_with_index { |book, index| puts "#{index} Title: #{book.title}, Author: #{book.author}" }
+        book_index = gets.chomp.to_i
+        puts 'Select a person from the following list by number (not ID)'
+        @people.each_with_index { |person, index| puts "#{index} [#{person.class} Name: #{person.name}, ID: #{person.id}", "Age: #{person.age}]" }
+        person_index = gets.chomp.to_i
+        puts 'Date:'
+        date = gets.chomp
+        @rentals.push(Rental.new(date, @books[book_index], @people[person_index]))
+        puts 'Rental created successfully'
+        # here will call the main menu
+    end
 end
