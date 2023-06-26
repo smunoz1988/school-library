a215428b27abab20bad263ee14869dc84863dcbe
Daniela Moreno <dani9292@hotmail.com>
2023-06-26 17:10:00 -0500

    fix linters

--- rental_option.rb
+++ rental_option.rb
@@ -7,6 +7,7 @@ class RentalOption
     @books = books
     @people = people
   end
+
   def create_rental
     puts 'Select a book from the following list by number'
     @books.each_with_index { |book, index| puts "#{index} Title: #{book.title}, Author: #{book.author}" }
@@ -22,6 +23,7 @@ class RentalOption
     @rentals.push(Rental.new(date, @books[book_index], @people[person_index]))
     puts 'Rental created successfully'
   end
+
   def list_rentals
     puts 'Id of the person:'
     person_id = gets.chomp.to_i
