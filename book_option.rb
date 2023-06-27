ffe1737adb8ce64a61d97a6d930f701047512115
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-27 15:41:00 -0500

    create save_books method

--- book_option.rb
+++ book_option.rb
@@ -1,4 +1,5 @@
 require_relative 'book'
+require 'json'
 
 class BookOption
   attr_accessor :books
@@ -23,5 +24,20 @@ class BookOption
     author = gets.chomp
     @books.push(Book.new(title, author))
     puts "Book #{title} was created"
+  end
+
+  def save_books
+    books_data = []
+    @books.each do |book|
+      books_data.push(
+        title: book.title,
+        author: book.author,
+        rentals: book.rentals
+      )
+    end
+    # save to file books.json
+    File.open('books.json', 'w') do |f|
+      f.write(books_data.to_json)
+    end
   end
 end
