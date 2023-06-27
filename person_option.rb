6bc65fb0210e60c8d013be72342fd0cb3e3ed719
Daniela Moreno <dani9292@hotmail.com>
2023-06-27 17:04:54 -0500

    remove person print

--- person_option.rb
+++ person_option.rb
@@ -16,7 +16,6 @@ class PersonOption
       puts 'List of people:'
       @people.each do |person|
         puts "[#{person.class} Name: #{person.name}, ID: #{person.id}", "Age: #{person.age}]"
-        p person
       end
     end
   end
