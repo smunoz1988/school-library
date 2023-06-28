7aba1865d5cd625b0c9e6e5f0c2aa1f8858de7a0
Daniela Moreno <dani9292@hotmail.com>
2023-06-28 14:30:06 -0500

    Add return method can_use_service

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,24 +1,29 @@
 require './teacher'
 
 describe Teacher do
-    let(:teacher) { Teacher.new(34, 'Math', 'Jose') }
+  let(:teacher) { Teacher.new(34, 'Math', 'Jose') }
 
-    describe '#new' do
-        it 'creates a new teacher' do
-            expect(teacher).to be_an_instance_of(Teacher)
-        end
+  describe '#new' do
+    it 'creates a new teacher' do
+      expect(teacher).to be_an_instance_of(Teacher)
     end
+  end
 
-    describe '#name' do
-        it 'returns the correct name' do
-            expect(teacher.name).to eq('Jose')
-        end
+  describe '#name' do
+    it 'returns the correct name' do
+      expect(teacher.name).to eq('Jose')
     end
+  end
 
-    describe '#age' do
-        it 'returns the correct age' do
-            expect(teacher.age).to eq(34)
-        end
+  describe '#age' do
+    it 'returns the correct age' do
+      expect(teacher.age).to eq(34)
     end
-   
+  end
+
+  describe '#can_use_services?' do
+    it 'returns true' do
+      expect(teacher.can_use_services?).to be(true)
+    end
+  end
 end
