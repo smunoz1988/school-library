2761ec5c5aff38289596bb29b82cff9462b2a7f6
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-28 16:36:32 -0500

    add test to return the correct label

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -10,4 +10,10 @@ describe ClassRoom do
             expect(@classroom).to be_an_instance_of(ClassRoom)
         end
     end
+
+    describe '#label' do
+        it 'returns the correct label' do
+          expect(@classroom.label).to eq('Classroom 1')
+        end
+    end    
 end
