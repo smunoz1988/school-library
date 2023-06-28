6cc1666e9fbe9956398fd90284aae5a0de300fed
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-06-28 16:36:15 -0500

    add #new test to ClassRoom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -4,4 +4,10 @@ describe ClassRoom do
     before do
         @classroom = ClassRoom.new('Classroom 1')
     end
+
+    describe "#new" do
+        it "takes parameters and returns a Classroom object" do
+            expect(@classroom).to be_an_instance_of(ClassRoom)
+        end
+    end
 end
